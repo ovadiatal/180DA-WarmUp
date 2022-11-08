50f45c1bc0a3f1f22d07e346fac3f8b9394e3069
ovadiatal <tovadia@g.ucla.edu>
2022-11-07 19:42:59 -0800

    recognize start and end

--- voice_command.py
+++ voice_command.py
@@ -10,20 +10,22 @@ with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
 
-# recognize speech using Sphinx
-try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
-
 # recognize speech using Google Speech Recognition
 try:
     # for testing purposes, we're just using the default API key
     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
     # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+
+    command = r.recognize_google(audio)
+
+    print("Google Speech Recognition thinks you said " + command)
+
+    if "start" in command:
+        print("Start Recording")
+
+    if "end" in command:
+        print("End Recording")
+        
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
