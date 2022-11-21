4e4158bcb72e3f55743a7b6901aedab42cfd31cc
ovadiatal <tovadia@g.ucla.edu>
2022-11-21 11:15:53 -0800

    voice command camera integration

--- voice_command.py
+++ voice_command.py
@@ -5,52 +5,119 @@
 import speech_recognition as sr
 import cv2
 import numpy as np
-import multiprocessing
+import threading
+import time
+import os
 
 # function for response to command
+command = "m"
 
-def respond(command):
-    if "start" in command:
-        print("Start Recording")
-        cap = cv2.VideoCapture(0)
+# The state variable will change depending on whether recording is on or not
+control = "off"
+def respond():
+    global command
+    global control
+    while(True):
+        time.sleep(2)
+        print("before " + control)
+        if "start" in command:
+            print("Start Recording")
+            global cap
+            cap = cv2.VideoCapture(0)
+            control = "on"
+            print("in start " + control)
 
-        while(True):
-            # Capture frame-by-frame
-            ret, frame = cap.read()
+            while(True):
+                if "stop" in command:
+                    print("Stop Recording")
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    time.sleep(3)
+                    control = "off"
+                    break
+                #    break
+                # Capture frame-by-frame
+                ret, frame = cap.read()
 
-            # Display the resulting frame
-            cv2.imshow('frame',frame)
-            if cv2.waitKey(5) & 0xFF == 27:
-                break
-        # When everything done, release the capture
-        cap.release()
-        cv2.destroyAllWindows()
+                # Display the resulting frame
+                cv2.imshow('frame',frame)
+                if cv2.waitKey(5) & 0xFF == 27:
+                    break
+            # When everything done, release the capture
+            print("done? " + control)
+            cap.release()
+            cv2.destroyAllWindows()
 
+        #elif "stop" or "end" in command:
+         #   print("Stop Recording")
+                #cap.release()
+                #cv2.destroyAllWindows()
+          #  time.sleep(3)
+        #else:
+         #   print("waiting for command")
+          #  time.sleep(1)
 
 # function to translate audio to command
 
-def hear(audio):
-    command = r.recognize_google(audio)
-    print("Google Speech Recognition thinks you said " + command)
-    return command
+def hear():
+    
+    while(True):
+        
+        global command
+
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = r.listen(source)
+
+        try:
+            command = r.recognize_google(audio)
+            print("Google Speech Recognition thinks you said " + command)
+
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+        time.sleep(3)
+        
+
+        #print("hear")
+        #time.sleep(2)
+
+def print_command():
+    global command
+    print("entered")
+    while(True):
+        print("you ordered" + command)
+        time.sleep(3)
+
+
+
 
 
 
 # recognize speech using Google Speech Recognition
-try:
+# try:
 
-    command = hear(audio)
-    respond(command)
+if __name__ == '__main__':
+    t1 = threading.Thread(target=hear)
+    t2 = threading.Thread(target=respond)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+            
+            
+
+        
+       # p = mp.Process(target=respond,args=(command))
 
 
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+# except sr.UnknownValueError:
+#     print("Google Speech Recognition could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from Google Speech Recognition service; {0}".format(e))
